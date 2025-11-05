b878aa3fee89abdb0b381e46f85b3309daf783d9
Abir Abdullah <abirabdullah3491@gmail.com>
2025-11-05 12:14:11 +0600

    ok

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,30 +25,33 @@ const Navbar = () => {
     const { user } = useAuth();
     // console.log(user);
     const links = <>
-        <NavLink to={'/'}
-            className={({ isActive }) =>
-                isActive ? "text-purple-600 font-semibold" : ""
-            }>
-            <li>Home</li>
-        </NavLink>
-        <NavLink to={user ? "/profile" : "/login"}
-            className={({ isActive }) =>
-                isActive ? "text-purple-600 font-semibold" : ""
-            }>
-            <li>My Profile</li>
-        </NavLink>
+        <li>
+            <NavLink to={'/'}
+                className={({ isActive }) =>
+                    isActive ? "text-purple-600 font-semibold" : ""
+                }>
+                Home
+            </NavLink>
+        </li>
+        <li>
+            <NavLink to={user ? "/profile" : "/login"}
+                className={({ isActive }) =>
+                    isActive ? "text-purple-600 font-semibold" : ""
+                }>
+                My Profile
+            </NavLink>
+        </li>
     </>
     return (
         <div>
             <div className="navbar bg-base-100 shadow-sm">
                 <div className="navbar-start">
                     <div className="dropdown">
-                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
-                        </div>
+                        </label>
                         <ul
-                            tabIndex="-1"
-                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-50 mt-3 w-52 p-2 shadow">
                             {links}
                         </ul>
                     </div>
